fc7f995494fb0d56dbd88960db0b9b23c258b367
BrookeHau <brookerhauser@gmail.com>
2018-03-19 14:50:04 -0400

    working through testing

---
+++ src/main/java/products/ProductController.java
@@ -0,0 +1,13 @@
+package products;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+	@RequestMapping
+	public void foo() {
+		
+	}
+}
